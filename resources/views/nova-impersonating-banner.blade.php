804ce97193bdd44f8dbac7561db0ad78dda92ed7
Lucas Rolff <lucas@slcoding.com>
2025-04-04 21:44:08 +0200

    Update nova-impersonating-banner.blade.php

--- resources/views/nova-impersonating-banner.blade.php
+++ resources/views/nova-impersonating-banner.blade.php
@@ -3,12 +3,12 @@
         <div class="items-center justify-center w-full p-2 text-center text-white bg-red-600 md:flex">
             <p class="text-lg">You are impersonating</p>
 
-            <div class="flex-grow">
+            <div class="grow">
                 <p class="text-sm">{{ $impersonating->id }} - {{ $impersonating->name }}</p>
                 <p class="text-sm">{{ $impersonating->email }}</p>
             </div>
 
-            <button wire:click.prevent="stopImpersonating" class="inline-flex items-center px-4 py-2 ml-auto text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25">Stop Impersonating</button>
+            <button wire:click="stopImpersonating" class="inline-flex items-center px-4 py-2 ml-auto text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-hidden focus:border-gray-900 focus:ring-3 focus:ring-gray-300 disabled:opacity-25">Stop Impersonating</button>
         </div>
     @endif
 </div>
